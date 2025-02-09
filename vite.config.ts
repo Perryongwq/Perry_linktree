03c2364933225367ada4a19b907497dc370303a5
Perry Ong <perryongwq@gmail.com>
2025-02-09 23:53:01 +0800

    Add CI/CD GitHub Actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  base: '/Perry_linktree/',  // <--- Add this line
   plugins: [react()],
+  resolve: {
+    alias: {
+      // The key is "@" and the value is the absolute path to your `src` folder
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
